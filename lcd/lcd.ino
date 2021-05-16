846492b0bcfe4685b6f66e35f19397ac9fb7a2f3
bontorhumala <bontorhumala@github.com>
2021-05-16 22:09:52 +0700

    first commit

---
+++ lcd/lcd.ino
@@ -0,0 +1,44 @@
+/*
+  Freelab - LCD 1602
+
+ Print "Hello World!" pada LCD
+
+ Rangkaian:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to GND
+ * LCD VSS pin to GND
+ * LCD VCC pin to +5V
+ * 10K potensiometer: pin 1 dan 3 ke +5V dan GND, pin 2 ke LCD pin 3
+
+ https://www.arduino.cc/en/Tutorial/HelloWorld
+
+*/
+
+// include the library code:
+#include <LiquidCrystal.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("Hello, World!");
+}
+
+void loop() {
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(millis() / 1000);
+}
+
